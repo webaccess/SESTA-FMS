28f3611a3300040c9d6df1830c250a427fb03172
Suyesh Tiwari <suyesh.tiwari@webaccessglobal.com>
2020-02-26 12:16:02 +0530

    Snackbar component with Alert

--- webapp/src/components/UI/Snackbar/Snackbar.js
+++ webapp/src/components/UI/Snackbar/Snackbar.js
@@ -2,57 +2,57 @@
  * SnackBar
  * Snackbars provide brief messages about app processes.
  * The component is also known as a toast.
-
+severity="error"
+severity="warning"
+severity="info"
+severity="success"
 
 **Sample code for using Snackbar**
+  <Snackbar open={BOOLEAN}  severity="error" autoHideDuration={6000} onClose={EVENT}> 
+    This is a success message!
+  </Snackbar>
+**/
 
+import React, { useState } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import style from './Snackbar.module.css'
+import MuiAlert from '@material-ui/lab/Alert';
+import { useTheme } from "@material-ui/styles";
+import { useMediaQuery } from "@material-ui/core";
+import Aux from "../../../hoc/Auxiliary/Auxiliary.js";
+
+export default function CustomizedSnackbars(props) {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"), {
+    defaultMatches: true
+  });
+  const [showMobileSnackbar, setshowMobileSnackbar] = useState(true);
+
+  const closeMobileSnackbar = () => {
+    setshowMobileSnackbar(false);
+  };
+
+
+  const openDesktopSnackbar = isDesktop ? false : showMobileSnackbar;
 
   function Alert(props) {
     return <MuiAlert elevation={6}  {...props} />;
   }
-  const [open, setOpen] = React.useState(true);
-
-  {OPTIONAL}  const handleClick = () => {
-                setOpen(true);
-              };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  
-  <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-    <Alert severity="error" >
-      This is a success message!
-    </Alert>
-  </Snackbar>
-
-  <div className={styles.Snackbardesktop}>
-    <Alert style={{ width: '600px' }} severity="error" >
-      This is a success message!
-    </Alert>
-
-
- CSS for Desktop mode Snackbar !!!IMPORTANT
-@media (max-width: 500px) {
-  .Snackbardesktop {
-    display: none;
-    width: 500px;
-  }
-}
-
-**/
-
-import React from 'react';
-import Snackbar from '@material-ui/core/Snackbar';
-import style from './Snackbar.module.css'
-
-export default function CustomizedSnackbars(props) {
 
   return (
-    <div className={style.Snackbar}>
-      <Snackbar open={props.open} autoHideDuration={6000} onClose={props.onClose}>
-        {props.children}
-      </Snackbar>
-    </div>
+    <Aux>
+      <div >
+        <Snackbar open={openDesktopSnackbar} autoHideDuration={6000} onClose={closeMobileSnackbar}>
+          <Alert severity={props.severity}>
+            {props.children}
+          </Alert>
+        </Snackbar>
+      </div>
+      <div className={isDesktop ? "" : style.Hidden}>
+        <Alert style={{ width: "500px" }} severity={props.severity}>
+          {props.children}
+        </Alert>
+      </div>
+    </Aux>
   );
 }
