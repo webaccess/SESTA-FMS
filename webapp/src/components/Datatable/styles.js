70341cd446d50449c23e8c1a6cec2b4c32eed72a
Suyesh Tiwari <suyesh.tiwari@webaccessglobal.com>
2020-02-27 15:42:52 +0530

    Datatable component with Theme and Search Filter

---
+++ webapp/src/components/Datatable/styles.js
@@ -0,0 +1,239 @@
+import merge from 'deepmerge';
+import { defaultThemes } from './themes.js';
+
+export const defaultStyles = theme => ({
+  table: {
+    style: {
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+    },
+  },
+  tableWrapper: {
+    style: {
+      display: 'table',
+    },
+  },
+  header: {
+    style: {
+      fontSize: '22px',
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+      minHeight: '56px',
+      paddingLeft: '16px',
+      paddingRight: '8px',
+    },
+  },
+  subHeader: {
+    style: {
+      backgroundColor: theme.background.default,
+      minHeight: '52px',
+    },
+  },
+  head: {
+    style: {
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  headRow: {
+    style: {
+      backgroundColor: '#FFFFFF',
+      minHeight: '56px',
+      borderBottomWidth: '10px',
+      borderBottomColor: theme.divider.default,
+      borderBottomStyle: 'solid',
+    },
+    denseStyle: {
+      minHeight: '32px',
+    },
+  },
+  headCells: {
+    style: {
+      fontSize: '1px',
+      fontWeight: 500,
+      color: theme.text.primary,
+      paddingLeft: '16px',
+      paddingRight: '16px',
+    },
+    activeSortStyle: {
+      color: theme.text.primary,
+      '&:focus': {
+        outline: 'none',
+      },
+      '&:hover:not(:focus)': {
+        color: theme.sortFocus.default,
+      },
+    },
+    inactiveSortStyle: {
+      '&:focus': {
+        outline: 'none',
+        color: theme.sortFocus.default,
+      },
+      '&:hover': {
+        color: theme.sortFocus.default,
+      },
+    },
+  },
+  contextMenu: {
+    style: {
+      backgroundColor: theme.context.background,
+      fontSize: '18px',
+      fontWeight: 400,
+      color: theme.context.text,
+      paddingLeft: '16px',
+      paddingRight: '8px',
+      transform: 'translate3d(0, -100%, 0)',
+      transitionDuration: '125ms',
+      transitionTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+      willChange: 'transform',
+    },
+    activeStyle: {
+      transform: 'translate3d(0, 0, 0)',
+    },
+  },
+  cells: {
+    style: {
+      paddingLeft: '16px',
+      paddingRight: '16px',
+      wordBreak: 'break-word',
+    },
+  },
+  rows: {
+    style: {
+      fontSize: '13px',
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+      minHeight: '48px',
+      '&:not(:last-of-type)': {
+        borderBottomStyle: 'solid',
+        borderBottomWidth: '1px',
+        borderBottomColor: theme.divider.default,
+      },
+    },
+    denseStyle: {
+      minHeight: '32px',
+    },
+    selectedHighlighStyle: {
+      color: theme.selected.text,
+      backgroundColor: theme.selected.default,
+      '&:not(:last-of-type)': {
+        borderBottomColor: theme.background.default,
+      },
+    },
+    highlightOnHoverStyle: {
+      color: theme.highlightOnHover.text,
+      backgroundColor: theme.highlightOnHover.default,
+      transitionDuration: '0.15s',
+      transitionProperty: 'background-color',
+      '&:not(:last-of-type)': {
+        borderBottomColor: theme.background.default,
+        outlineStyle: 'solid',
+        outlineWidth: '1px',
+        outlineColor: theme.background.default,
+      },
+    },
+    stripedStyle: {
+      '&:nth-child(odd)': {
+        color: theme.striped.text,
+        backgroundColor: theme.striped.default,
+      },
+    },
+  },
+  expanderRow: {
+    style: {
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+    },
+  },
+  expanderCell: {
+    style: {
+      flex: '0 0 48px',
+    },
+  },
+  expanderButton: {
+    style: {
+      color: theme.button.default,
+      fill: theme.button.default,
+      backgroundColor: 'transparent',
+      borderRadius: '2px',
+      transition: '0.25s',
+      height: '100%',
+      width: '100%',
+      '&:hover:enabled': {
+        cursor: 'pointer',
+      },
+      '&:disabled': {
+        color: theme.button.disabled,
+      },
+      '&:hover:not(:disabled)': {
+        cursor: 'pointer',
+        backgroundColor: theme.button.hover,
+      },
+      '&:focus': {
+        outline: 'none',
+        backgroundColor: theme.button.focus,
+      },
+      svg: {
+        margin: 'auto',
+      },
+    },
+  },
+  pagination: {
+    style: {
+      color: theme.text.secondary,
+      fontSize: '13px',
+      minHeight: '56px',
+      backgroundColor: theme.background.default,
+      borderTopStyle: 'solid',
+      borderTopWidth: '1px',
+      borderTopColor: theme.divider.default,
+    },
+    pageButtonsStyle: {
+      borderRadius: '50%',
+      height: '40px',
+      width: '40px',
+      padding: '8px',
+      margin: 'px',
+      cursor: 'pointer',
+      transition: '0.4s',
+      color: theme.button.default,
+      fill: theme.button.default,
+      backgroundColor: 'transparent',
+      '&:disabled': {
+        opacity: '0.4',
+        cursor: 'unset',
+        color: theme.button.disabled,
+      },
+      '&:hover:not(:disabled)': {
+        backgroundColor: theme.button.hover,
+      },
+      '&:focus': {
+        outline: 'none',
+        backgroundColor: theme.button.focus,
+      },
+    },
+  },
+  noData: {
+    style: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+    },
+  },
+  progress: {
+    style: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: theme.text.primary,
+      backgroundColor: theme.background.default,
+    },
+  },
+});
+
+export const createStyles = (customStyles = {}, theme = 'default') => {
+  const themeType = defaultThemes[theme] ? theme : 'default';
+
+  return merge(defaultStyles(defaultThemes[themeType]), customStyles);
+};
