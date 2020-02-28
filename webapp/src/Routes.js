624c41a225fec28f30667647f6324164b8c21db5
Suyesh Tiwari <suyesh.tiwari@webaccessglobal.com>
2020-02-28 16:44:07 +0530

    Changes

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -7,7 +7,6 @@ import PrivateRoute from "./hoc/PrivateRoute/PrivateRoute";
 import AuthRoute from "./hoc/AuthRoute/AuthRoute";
 import Dashboard from "./containers/Dashboard/Dashboard";
 import NotFoundPage from "./containers/NotFoundPage/NotFoundPage";
-import Users from "./containers/Users/users.js"
 import AuthPage from "./containers/AuthPage/AuthPage";
 
 function Routes() {
@@ -17,7 +16,6 @@ function Routes() {
         <div>
           <Switch>
             <PrivateRoute path="/" component={Dashboard} exact />
-            <PrivateRoute path="/Users/add" component={Users} exact />
             <Route path="/404" component={NotFoundPage} />
             <AuthRoute path="/:authType/:id?" component={AuthPage} />
           </Switch>
