a629c2d1dc9f5d83c13806adb21859bc4616754e
Web Access <tanvi.salkar@webaccessglobal.com>
2020-02-28 13:20:40 +0530

    basic form done

--- webapp/src/containers/Users/Users.js
+++ webapp/src/containers/Users/Users.js
@@ -12,25 +12,90 @@ import {
   FormControlLabel,
   Checkbox
 } from "@material-ui/core";
+import { map } from "lodash";
+import validateInput from "../../components/Validation/ValidateInput/ValidateInput";
+
+const states = [
+  {
+    value: "alabama",
+    label: "Alabama"
+  },
+  {
+    value: "new-york",
+    label: "New York"
+  },
+  {
+    value: "san-francisco",
+    label: "San Francisco"
+  }
+];
 
 class Users extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      values: {
-        firstName: "",
-        lastName: "",
-        username: "",
-        email: "",
-        phone: "",
-        country: ""
-      }
+      values: {},
+      validations: {
+        firstName: {
+          required: { value: "true", message: "firstname field required" }
+        },
+        lastName: {},
+        username: {},
+        email: {},
+        phone: {},
+        state: {}
+      },
+      errors: {
+        firstName: [],
+        lastName: [],
+        username: [],
+        email: [],
+        phone: [],
+        state: []
+      },
+      serverErrors: {},
+      formSubmitted: false
     };
   }
 
   handleChange = ({ target }) => {
     this.setState({
-      value: { ...this.state.value, [target.name]: target.value }
+      values: { ...this.state.values, [target.name]: target.value }
+    });
+  };
+
+  validate = () => {
+    const values = this.state.values;
+    const validations = this.state.validations;
+    map(validations, (validation, key) => {
+      let value = values[key] ? values[key] : "";
+      const errors = validateInput(value, validation);
+
+      let errorset = this.state.errors;
+      if (errors.length > 0) errorset[key] = errors;
+      else delete errorset[key];
+      this.setState({ errors: errorset });
+    });
+  };
+
+  hasError = field => {
+    return Object.keys(this.state.errors).length > 0 &&
+      this.state.errors[field].length > 0
+      ? true
+      : false;
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.validate();
+
+    if (Object.keys(this.state.errors).length > 0) return;
+    //ajax call
+  };
+
+  resetForm = () => {
+    this.setState({
+      values: {}
     });
   };
 
@@ -38,7 +103,12 @@ class Users extends Component {
     return (
       <Layout>
         <Card>
-          <form autoComplete="off" noValidate>
+          <form
+            autoComplete="off"
+            noValidate
+            onSubmit={this.handleSubmit}
+            method="post"
+          >
             <CardHeader
               subheader="The information can be edited"
               title="Profile"
@@ -49,13 +119,17 @@ class Users extends Component {
                 <Grid item md={6} xs={12}>
                   <Input
                     fullWidth
-                    helperText="Please specify the first name"
                     label="First name"
                     margin="dense"
                     name="firstName"
+                    error={this.hasError("firstName")}
+                    helperText={
+                      this.hasError("firstName")
+                        ? this.state.errors.firstName[0]
+                        : null
+                    }
+                    value={this.state.values.firstName || ""}
                     onChange={this.handleChange}
-                    required
-                    value={this.state.values.firstName}
                     variant="outlined"
                   />
                 </Grid>
@@ -67,7 +141,7 @@ class Users extends Component {
                     name="lastName"
                     onChange={this.handleChange}
                     required
-                    value={this.state.values.lastName}
+                    value={this.state.values.lastName || ""}
                     variant="outlined"
                   />
                 </Grid>
@@ -79,7 +153,7 @@ class Users extends Component {
                     name="username"
                     onChange={this.handleChange}
                     required
-                    value={this.state.values.username}
+                    value={this.state.values.username || ""}
                     variant="outlined"
                   />
                 </Grid>
@@ -91,7 +165,7 @@ class Users extends Component {
                     name="email"
                     onChange={this.handleChange}
                     required
-                    value={this.state.values.email}
+                    value={this.state.values.email || ""}
                     variant="outlined"
                   />
                 </Grid>
@@ -103,26 +177,45 @@ class Users extends Component {
                     name="phone"
                     onChange={this.handleChange}
                     type="number"
-                    value={this.state.values.phone}
+                    value={this.state.values.phone || ""}
                     variant="outlined"
                   />
                 </Grid>
                 <Grid item md={6} xs={12}>
                   <FormControlLabel
-                    control={
-                      <Checkbox
-                        color="primary"
-                        defaultChecked //
-                      />
-                    }
+                    control={<Checkbox color="primary" defaultChecked />}
                     label="Email"
                   />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <Input
+                    fullWidth
+                    label="Select State"
+                    helperText="Please select the state first"
+                    margin="dense"
+                    name="state"
+                    onChange={this.handleChange}
+                    required
+                    select
+                    // eslint-disable-next-line react/jsx-sort-props
+                    value={this.state.values.state || ""}
+                    variant="outlined"
+                  >
+                    {states.map(option => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </Input>
                 </Grid>
               </Grid>
             </CardContent>
             <Divider />
             <CardActions>
-              <Button>Save details</Button>
+              <Button type="submit">Save</Button>
+              <Button color="default" clicked={this.resetForm}>
+                Reset
+              </Button>
             </CardActions>
           </form>
         </Card>
