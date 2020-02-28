86192c7b2178c0bb00294e5bcf836ec20d712f4c
Suyesh Tiwari <suyesh.tiwari@webaccessglobal.com>
2020-02-28 16:39:08 +0530

    Routings to all Components

---
+++ webapp/src/Routes.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, useLocation, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Aux from './hoc/Auxiliary/Auxiliary.js';
+import PrivateRoute from "./hoc/PrivateRoute/PrivateRoute";
+import AuthRoute from "./hoc/AuthRoute/AuthRoute";
+import Dashboard from "./containers/Dashboard/Dashboard";
+import NotFoundPage from "./containers/NotFoundPage/NotFoundPage";
+import Users from "./containers/Users/users.js"
+import AuthPage from "./containers/AuthPage/AuthPage";
+
+function Routes() {
+  return (
+    <Aux>
+      <Router>
+        <div>
+          <Switch>
+            <PrivateRoute path="/" component={Dashboard} exact />
+            <PrivateRoute path="/Users/add" component={Users} exact />
+            <Route path="/404" component={NotFoundPage} />
+            <AuthRoute path="/:authType/:id?" component={AuthPage} />
+          </Switch>
+        </div>
+      </Router>
+    </Aux>
+  );
+}
+
+export default Routes;
+
+
+
+
