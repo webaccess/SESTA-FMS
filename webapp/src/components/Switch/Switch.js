eee1c2604a7304eae82223bb1ffa4cc11aaf68ca
SuyeshTiwari165 <suyeshtiwari123@gmail.com>
2020-04-06 20:32:38 +0530

    chnages made in switch component

--- webapp/src/components/Switch/Switch.js
+++ webapp/src/components/Switch/Switch.js
@@ -1,42 +1,71 @@
-/*
-Toggle Switch Component
-Note: id is required for ToggleSwitch component to function. Name, currentValue, defaultChecked, Small and onChange're optional.
-Usage: <Switch id=id onChange={e=>{function(e)}}/>
-*/
+// /*
+// Toggle Switch Component
+// Note: id is required for ToggleSwitch component to function. Name, currentValue, defaultChecked, Small and onChange're optional.
+// Usage: <Switch id=id onChange={e=>{function(e)}}/>
+// */
 
-import React, { useState } from "react";
+import React, { Component ,useState} from "react";
+import { withStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
 import PropTypes from "prop-types";
-import style from "./Switch.module.css"
+import '../../App.css';
 
-function Switch(props) {
-  const [state, setState] = useState(false)
-  state = {
-    checked: this.props.defaultChecked
-  };
+function ToggleSwitch(props){
+const [state, setState] = useState({
+checked: props.defaultChecked
+});
+
   const onChange = e => {
-    this.setState({
+    setState({
       checked: e.target.checked
     });
-    console.log("ddd", e.target)
-    if (typeof this.props.onChange === "function") this.props.onChange();
+    if (typeof props.onChange === "function") props.onChange();
   };
-  return (
-    <div>
-      <input
-        type="checkbox"
-        name={props.name}
-        className="toggle-switch-checkbox"
-        id={props.id}
-        checked={props.currentValue}
-        defaultChecked={props.defaultChecked}
-        onChange={onChange}
-        disabled={props.disabled}
-      />
-    </div>
-  );
-}
 
-Switch.propTypes = {
+    return (
+      <div
+        className={"toggle-switch" + (props.Small ? " small-switch" : "")}
+      >
+        <input
+          type="checkbox"
+          name={props.Name}
+          className="toggle-switch-checkbox"
+          id={props.id}
+          checked={props.currentValue}
+          defaultChecked={props.defaultChecked}
+          onChange={props.onChange}
+          disabled={props.disabled}
+        />
+        {props.id ? (
+          <label className="toggle-switch-label" htmlFor={props.id}>
+            <span
+              className={
+                props.disabled
+                  ? "toggle-switch-inner toggle-switch-disabled"
+                  : "toggle-switch-inner"
+              }
+              // data-yes={props.Text[0]}
+              // data-no={props.Text[1]}
+            />
+            <span
+              className={
+                props.disabled
+                  ? "toggle-switch-switch toggle-switch-disabled"
+                  : "toggle-switch-switch"
+              }
+            />
+          </label>
+        ) : null}
+      </div>
+    );
+  }
+  // Set text for rendering.
+  // static defaultProps = {
+  //   Text: ["Yes", "No"]
+  // };
+
+
+ToggleSwitch.propTypes = {
   id: PropTypes.string.isRequired,
   Text: PropTypes.string.isRequired,
   Name: PropTypes.string,
@@ -47,4 +76,4 @@ Switch.propTypes = {
   disabled: PropTypes.bool
 };
 
-export default Switch;
+export default ToggleSwitch;
