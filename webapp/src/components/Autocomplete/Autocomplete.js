94ebec466f7712372ab61049c66f29d4bd0121a0
Tanvi <tanviGanesh@users.noreply.github.com>
2020-04-09 15:12:36 +0530

    autocomplete component updated

--- webapp/src/components/Autocomplete/Autocomplete.js
+++ webapp/src/components/Autocomplete/Autocomplete.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import Chip from '@material-ui/core/Chip';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: 500,
-    '& > * + *': {
-      marginTop: theme.spacing(3),
-    },
-  },
-}));
+import React from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export default function Tags(props) {
-  const classes = useStyles();
-
   return (
     <Autocomplete
       multiple={props.multiple}
@@ -23,18 +9,10 @@ export default function Tags(props) {
       options={props.options}
       getOptionLabel={props.getOptionLabel}
       onChange={props.onChange}
+      defaultValue={props.defaultValue ? props.defaultValue : []}
+      value={props.value ? props.value : []}
       filterSelectedOptions
-      renderInput={params => (
-        <TextField
-          {...params}
-          variant={props.variant}
-          label={props.label}
-          placeholder={props.placeholder}
-          name={props.name}
-          helperText={props.helperText}
-          error={props.error}
-        />
-      )}
+      renderInput={props.renderInput}
     />
   );
 }
