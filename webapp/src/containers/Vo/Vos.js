4916213fc03a1d23bad5c7c73dc136fafe68f7ad
Ankitashinde0 <ankitashinde0807@gmail.com>
2020-04-13 11:32:24 +0530

    Removed migration scripts changes #73 #51

--- webapp/src/containers/Vo/Vos.js
+++ webapp/src/containers/Vo/Vos.js
@@ -83,25 +83,25 @@ export class Vos extends React.Component {
 
   async componentDidMount() {
     await axios
-      .get(process.env.REACT_APP_SERVER_URL + "organizations/?sub_type='VO'"{
-        headers: {
-          Authorization: "Bearer " + auth.getToken() + "",
-        },
-      })
-      .then((res) => {
-        this.setState({ data: res.data });
-      });
-    //api call for states filter
-    await axios
       .get(
         process.env.REACT_APP_SERVER_URL +
           "village-organizations/?_sort=name:ASC",
         {
           headers: {
-            Authorization: "Bearer " + auth.getToken() + ""
-          }
+            Authorization: "Bearer " + auth.getToken() + "",
+          },
         }
       )
+      .then((res) => {
+        this.setState({ data: res.data });
+      });
+    //api call for states filter
+    await axios
+      .get(process.env.REACT_APP_SERVER_URL + "states?is_active=true", {
+        headers: {
+          Authorization: "Bearer " + auth.getToken() + "",
+        },
+      })
       .then((res) => {
         this.setState({ getState: res.data });
       })
