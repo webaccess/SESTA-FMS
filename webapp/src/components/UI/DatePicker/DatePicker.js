c0081faecce7989acee86acfbc63b4cce820c029
SuyeshTiwari165 <suyeshtiwari123@gmail.com>
2020-05-09 10:46:11 +0530

    DatePicker Component

---
+++ webapp/src/components/UI/DatePicker/DatePicker.js
@@ -0,0 +1,43 @@
+import 'date-fns';
+import React from 'react';
+import PropTypes from "prop-types";
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from "@material-ui/core/styles";
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import style from "./DatePicker.module.css";
+
+const DatePicker = ({...props}) => {
+  
+  return (
+    
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container>
+          <KeyboardDatePicker
+            id="date-picker-dialog"
+            label="Date"
+            format="MM/dd/yyyy"
+            value={props.value}
+            inputVariant="outlined"
+            onChange={props.onChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+       </Grid>
+    </MuiPickersUtilsProvider>
+  );
+}
+     DatePicker.propTypes = {
+      classes: PropTypes.object.isRequired,
+      labelText: PropTypes.node,
+      labelProps: PropTypes.object,
+      id: PropTypes.string.isRequired,
+      control: PropTypes.object,
+      onChange: PropTypes.func.isRequired,
+      variant: PropTypes.oneOf(['standard', 'outlined', 'filled']),
+};
+export default DatePicker;
