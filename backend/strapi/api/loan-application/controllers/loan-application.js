e7f7f8fe638e3eaf826641ac85de406dcaab9dc9
tanviGanesh <tanvisalkar@yahoo.in>
2020-07-21 16:11:41 +0530

    files changed

--- backend/strapi/api/loan-application/controllers/loan-application.js
+++ backend/strapi/api/loan-application/controllers/loan-application.js
@@ -10,13 +10,48 @@ const puppeteer = require("puppeteer");
 module.exports = {
   printPDF: async (ctx) => {
     try {
-      const content = fs.readFileSync(
+      var content = fs.readFileSync(
         path.resolve(__dirname, "../../../assets/files/Loan-Application.html"),
         "utf-8"
       );
+      var contentVal = content.replace(/{FPO_NAME}/g, "Tanvi");
+      // SHG_VILLAGE;
+      // SHG_NAME;
+      // VO_NAME;
+      // SHG_ACC_NO;
+      // SHG_BANK;
+      // SHG_BANK_BRANCH;
+      // SHG_BANK_IFSC;
+      // await fs.writeFileSync(
+      //   path.resolve(__dirname, "../../../assets/files/Loan-Application1.html"),
+      //   contentVal
+      // );
+      // console.log("content");
+      // return ctx.send(contentVal);
+      // fs.writeFile(
+      //   __dirname,
+      //   "../../../assets/files/Loan-Application1.html",
+      //   "utf-8",
+      //   contentVal,
+      //   function (err, data) {
+      //     if (err) {
+      //       return console.log(err);
+      //     }
+      //     console.log(data);
+      //   }
+      // );
+      // fs.writeFileSync(
+      //   path.resolve(__dirname, "../../../assets/files/Loan-Application1.html"),
+      //   contentVal,
+      //   "utf-8"
+      // );
+      // const contentObt = fs.readFileSync(
+      //   path.resolve(__dirname, "../../../assets/files/Loan-Application.html"),
+      //   "utf-8"
+      // );
       const browser = await puppeteer.launch({ headless: true });
       const page = await browser.newPage();
-      await page.setContent(content);
+      await page.setContent(contentVal);
       const buffer = await page.pdf({
         format: "A4",
         printBackground: true,
