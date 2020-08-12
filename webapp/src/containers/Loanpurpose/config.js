313cc46fddc861f2e745e74bd48c4b37a7974b0a
pawan200 <pawankumar.singh@webaccessglobal.com>
2020-08-12 09:52:19 +0530

    Final changes

---
+++ webapp/src/containers/Loanpurpose/config.js
@@ -0,0 +1,11 @@
+export const ADD_LOANPURPOSE_BREADCRUMBS = {
+  home: "Home",
+  list_loanpurpose: "Loan Purpose",
+  add_loanpurpose: "Add Loan Purpose",
+};
+
+export const EDIT_LOANPURPOSE_BREADCRUMBS = {
+  home: "Home",
+  list_loanpurpose: "Loan Purpose",
+  edit_loanpurpose: "Edit  Loan Purpose",
+};
