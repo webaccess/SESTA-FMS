23e617944f4b21325fb95551cad00c94509e815e
Ankita Shinde <ankitashinde0807@gmail.com>
2020-09-15 18:25:58 +0530

    Retrieving stashed files

--- backend/strapi/config/database.js
+++ backend/strapi/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: "postgres",
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "loancheck"),
+        database: env("DATABASE_NAME", "newSesta"),
         username: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "postgres"),
         schema: "public",
