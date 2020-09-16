8dbb3f864ec772be06cd2b317bae548fa18b78b8
Ankita Shinde <ankitashinde0807@gmail.com>
2020-09-16 21:47:22 +0530

    Removed comments

--- webapp/src/containers/Shgs/Shgs.js
+++ webapp/src/containers/Shgs/Shgs.js
@@ -369,17 +369,14 @@ export class Shgs extends React.Component {
         name: "State",
         selector: "addresses[1].state.name",
         sortable: true,
-        //cell: (row) => (row.addresses[1] ? addresses[1].state.name : "-"),
       },
       {
         name: "District",
         selector: "addresses[2].district.name",
         sortable: true,
-        //cell: (row) => (row.addresses[2] ? addresses[2].district.name : "-"),
       },
       {
         name: "Village",
-        //cell: (row) => (row.addresses[3] ? addresses[3].village.name : "-"),
         selector: "addresses[3].village.name",
         sortable: true,
       },
